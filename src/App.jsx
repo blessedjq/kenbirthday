65ccde91eacff7301409368fb114ff6a127d4dc8
Blessed Jeberson <sprexcel@Blesseds-Mac-mini.local>
2025-06-18 09:36:54 +0530

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 import "./App.css";
 import { motion, AnimatePresence } from "framer-motion";
 
-const BIRTHDAY = moment("2025-06-27T00:00:00");
+const BIRTHDAY = moment("2025-04-27T00:00:00");
 const SECRET_CODE = "kency2002";
 
 export default function App() {
